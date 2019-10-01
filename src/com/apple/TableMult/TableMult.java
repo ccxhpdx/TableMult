7893b5f028be731950c7db88d2e191b78908e0ab
panda <710126864@qq.com>
2019-10-02 02:56:51 +0800

    creat a TableMult class

---
+++ src/com/apple/TableMult/TableMult.java
@@ -0,0 +1,21 @@
+class TableMult
+{
+public static void main (String[] args)
+{
+    for(int i = 1; i <= 9; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            if(i == 3 && j == 3 || i == 4 && j == 3)
+            {
+                System.out.print(" "+j + "*" + i + "=" + i*j + "  "); 
+            }
+            else
+            {
+                System.out.print(j + "*" + i + "=" + i*j + "  ");
+            }
+        }
+        System.out.println();
+    }
+}
+}
